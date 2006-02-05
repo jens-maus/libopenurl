565966e24f1842763ae25154b11865b1a54cd3e0
Alexandre Balaban <alexandre@balaban.name>
2006-02-05 21:28:53 +0000

    ReAdd the OpenURL cmd sources, OS4 ready

---
+++ openurl/Cmd/OpenURL_rev.h
@@ -0,0 +1,9 @@
+#define PRG         "OpenURL"
+#define VERSION     5
+#define REVISION    0
+#define VRSTRING    "5.0"
+#define DATE        "20.3.2003"
+#define VERS        "OpenURL 5.0"
+#define VSTRING     "OpenURL 5.0 (20.3.2003)\r\n"
+#define VERSTAG     "\0$VER: OpenURL 5.0 (20.3.2003)"
+#define PRGNAME     "OpenURL 5.0 (20.3.2003)"
