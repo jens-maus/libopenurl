b1ed9a17ccb22710f5514f76b9734205adf9bf80
Jens Maus <mail@jens-maus.de>
2009-05-26 06:03:46 +0000

    - updated the developer include according to the very latest changes

--- developer/C/include/inline/openurl.h
+++ developer/C/include/inline/openurl.h
@@ -1,83 +1,83 @@
+/* Automatically generated header! Do not edit! */
+
 #ifndef _INLINE_OPENURL_H
 #define _INLINE_OPENURL_H
 
-#ifndef CLIB_OPENURL_PROTOS_H
-#define CLIB_OPENURL_PROTOS_H
-#endif
-
-#include <exec/types.h>
+#ifndef __INLINE_MACROS_H
+#include <inline/macros.h>
+#endif /* !__INLINE_MACROS_H */
 
 #ifndef OPENURL_BASE_NAME
 #define OPENURL_BASE_NAME OpenURLBase
-#endif
+#endif /* !OPENURL_BASE_NAME */
 
-#define URL_OpenA(url, tags) \
-	((ULONG (*)(STRPTR __asm("a0"), struct TagItem * __asm("a1"), struct Library * __asm("a6"))) \
-  (((char *) OPENURL_BASE_NAME) - 30))(url, tags, OPENURL_BASE_NAME)
+#define URL_FreePrefsA(___prefs, ___tags) \
+	LP2NR(0x4e, URL_FreePrefsA, struct URL_Prefs *, ___prefs, a0, struct TagItem *, ___tags, a1, \
+	, OPENURL_BASE_NAME)
 
 #ifndef NO_INLINE_STDARG
-#define URL_Open(url, tags...) \
-	({ULONG _tags[] = {tags}; URL_OpenA((url), (struct TagItem *) _tags);})
-#endif
+#define URL_FreePrefs(a0, ...) \
+	({ULONG _tags[] = { __VA_ARGS__ }; URL_FreePrefsA((a0), (struct TagItem *)_tags);})
+#endif /* !NO_INLINE_STDARG */
 
-#define URL_OldGetPrefs() \
-	((struct URL_Prefs * (*)(struct Library * __asm("a6"))) \
-  (((char *) OPENURL_BASE_NAME) - 36))(OPENURL_BASE_NAME)
+#define URL_GetAttr(___attr, ___storage) \
+	LP2(0x60, ULONG, URL_GetAttr, ULONG, ___attr, d0, ULONG *, ___storage, a0, \
+	, OPENURL_BASE_NAME)
 
-#define URL_OldFreePrefs(up) \
-	((void (*)(struct URL_Prefs * __asm("a0"), struct Library * __asm("a6"))) \
-  (((char *) OPENURL_BASE_NAME) - 42))(up, OPENURL_BASE_NAME)
+#define URL_GetPrefsA(___tags) \
+	LP1(0x48, struct URL_Prefs *, URL_GetPrefsA, struct TagItem *, ___tags, a0, \
+	, OPENURL_BASE_NAME)
 
-#define URL_OldSetPrefs(up, permanent) \
-	((ULONG (*)(struct URL_Prefs * __asm("a0"), BOOL __asm("d0"), struct Library * __asm("a6"))) \
-  (((char *) OPENURL_BASE_NAME) - 48))(up, permanent, OPENURL_BASE_NAME)
+#ifndef NO_INLINE_STDARG
+#define URL_GetPrefs(...) \
+	({ULONG _tags[] = { __VA_ARGS__ }; URL_GetPrefsA((struct TagItem *)_tags);})
+#endif /* !NO_INLINE_STDARG */
+
+#define URL_LaunchPrefsAppA(___tags) \
+	LP1(0x5a, ULONG, URL_LaunchPrefsAppA, struct TagItem *, ___tags, a0, \
+	, OPENURL_BASE_NAME)
+
+#ifndef NO_INLINE_STDARG
+#define URL_LaunchPrefsApp(...) \
+	({ULONG _tags[] = { __VA_ARGS__ }; URL_LaunchPrefsAppA((struct TagItem *)_tags);})
+#endif /* !NO_INLINE_STDARG */
+
+#define URL_OldFreePrefs(___up) \
+	LP1NR(0x2a, URL_OldFreePrefs, struct URL_Prefs *, ___up, a0, \
+	, OPENURL_BASE_NAME)
 
 #define URL_OldGetDefaultPrefs() \
-	((struct URL_Prefs * (*)(struct Library * __asm("a6"))) \
-  (((char *) OPENURL_BASE_NAME) - 54))(OPENURL_BASE_NAME)
+	LP0(0x36, struct URL_Prefs *, URL_OldGetDefaultPrefs, \
+	, OPENURL_BASE_NAME)
+
+#define URL_OldGetPrefs() \
+	LP0(0x24, struct URL_Prefs *, URL_OldGetPrefs, \
+	, OPENURL_BASE_NAME)
 
 #define URL_OldLaunchPrefsApp() \
-	((ULONG (*)(struct Library * __asm("a6"))) \
-  (((char *) OPENURL_BASE_NAME) - 60))(OPENURL_BASE_NAME)
+	LP0(0x3c, ULONG, URL_OldLaunchPrefsApp, \
+	, OPENURL_BASE_NAME)
 
-#define URL_GetPrefsA(tags) \
-	((struct URL_Prefs * (*)(struct TagItem * __asm("a0"), struct Library * __asm("a6"))) \
-  (((char *) OPENURL_BASE_NAME) - 72))(tags, OPENURL_BASE_NAME)
+#define URL_OldSetPrefs(___up, ___permanent) \
+	LP2(0x30, ULONG, URL_OldSetPrefs, struct URL_Prefs *, ___up, a0, BOOL, ___permanent, d0, \
+	, OPENURL_BASE_NAME)
 
-#ifndef NO_INLINE_STDARG
-#define URL_GetPrefs(tags...) \
-	({ULONG _tags[] = {tags}; URL_GetPrefsA((struct TagItem *) _tags);})
-#endif
-
-#define URL_FreePrefsA(prefs, tags) \
-	((void (*)(struct URL_Prefs * __asm("a0"), struct TagItem * __asm("a1"), struct Library * __asm("a6"))) \
-  (((char *) OPENURL_BASE_NAME) - 78))(prefs, tags, OPENURL_BASE_NAME)
+#define URL_OpenA(___url, ___tags) \
+	LP2(0x1e, ULONG, URL_OpenA, STRPTR, ___url, a0, struct TagItem *, ___tags, a1, \
+	, OPENURL_BASE_NAME)
 
 #ifndef NO_INLINE_STDARG
-#define URL_FreePrefs(prefs, tags...) \
-	({ULONG _tags[] = {tags}; URL_FreePrefsA((prefs), (struct TagItem *) _tags);})
-#endif
+#define URL_Open(a0, ...) \
+	({ULONG _tags[] = { __VA_ARGS__ }; URL_OpenA((a0), (struct TagItem *)_tags);})
+#endif /* !NO_INLINE_STDARG */
 
-#define URL_SetPrefsA(up, tags) \
-	((ULONG (*)(struct URL_Prefs * __asm("a0"), struct TagItem * __asm("a1"), struct Library * __asm("a6"))) \
-  (((char *) OPENURL_BASE_NAME) - 84))(up, tags, OPENURL_BASE_NAME)
-
-#ifndef NO_INLINE_STDARG
-#define URL_SetPrefs(up, tags...) \
-	({ULONG _tags[] = {tags}; URL_SetPrefsA((up), (struct TagItem *) _tags);})
-#endif
-
-#define URL_LaunchPrefsAppA(tags) \
-	((ULONG (*)(struct TagItem * __asm("a0"), struct Library * __asm("a6"))) \
-  (((char *) OPENURL_BASE_NAME) - 90))(tags, OPENURL_BASE_NAME)
+#define URL_SetPrefsA(___up, ___tags) \
+	LP2(0x54, ULONG, URL_SetPrefsA, struct URL_Prefs *, ___up, a0, struct TagItem *, ___tags, a1, \
+	, OPENURL_BASE_NAME)
 
 #ifndef NO_INLINE_STDARG
-#define URL_LaunchPrefsApp(tags...) \
-	({ULONG _tags[] = {tags}; URL_LaunchPrefsAppA((struct TagItem *) _tags);})
-#endif
+#define URL_SetPrefs(a0, ...) \
+	({ULONG _tags[] = { __VA_ARGS__ }; URL_SetPrefsA((a0), (struct TagItem *)_tags);})
+#endif /* !NO_INLINE_STDARG */
 
-#define URL_GetAttr(attr, storage) \
-	((ULONG (*)(ULONG __asm("d0"), ULONG * __asm("a0"), struct Library * __asm("a6"))) \
-  (((char *) OPENURL_BASE_NAME) - 96))(attr, storage, OPENURL_BASE_NAME)
-
-#endif /*  _INLINE_OPENURL_H  */
+#endif /* !_INLINE_OPENURL_H */
